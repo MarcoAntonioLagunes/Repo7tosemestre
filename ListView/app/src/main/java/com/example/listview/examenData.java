5356862d57890378df4515424cfad711d209793e
Marco Lagunes <202260487@ucc.mx>
2025-09-23 17:27:06 -0600

    Add files via upload

---
+++ ListView/app/src/main/java/com/example/listview/examenData.java
@@ -0,0 +1,14 @@
+java
+package com.example.recyclerview_ejemplo_gpob;
+
+public class examData {
+  String name;
+  String date;
+  String message;
+
+  public examData(String name, String date, String message) {
+    this.name = name;
+    this.date = date;
+    this.message = message;
+  }
+}
