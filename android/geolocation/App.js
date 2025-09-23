4f026fdd73c2fbda4a3d7d07e7e86661f86298ee
Marco Lagunes <202260487@ucc.mx>
2025-09-23 17:24:35 -0600

    Add files via upload

---
+++ android/geolocation/App.js
@@ -0,0 +1,31 @@
+import React,{userEffect} from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
+
+export default function App() {
+
+  const buscaLocation =async ()=>{
+
+  } 
+
+  userEffect(()=> {
+    buscaLocation()
+  })
+
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
